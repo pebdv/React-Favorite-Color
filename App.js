6cccc742065caa99a66dfb9ad53518efd202c237
pebdv <110866574+pebdv@users.noreply.github.com>
2022-08-08 20:51:12 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return <h1>My favorite color is {color}!</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FavoriteColor />);
+
+export default FavoriteColor;
